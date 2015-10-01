eafbe870b897864fc646405172de2e57d01aabcb
cstanwood <cstanwood@central1.com>
2015-10-01 15:49:50 -0700

    Nearly complete Board.java

--- Week4/src/main/java/Board.java
+++ Week4/src/main/java/Board.java
@@ -1,92 +1,249 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board
 {
-    private int[][] current;
-    private int[][] goal;
+	private int[][] blocks;
+	private int[][] goal;
+	private int moves = 0;
+	//	private Board previousBoard;
 
-    // construct a board from an N-by-N array of blocks
-    // (where blocks[i][j] = block in row i, column j)
-    public Board( int[][] blocks )
-    {
-        int length = blocks.length;
-        this.current = new int[ length ][ length ];
-        // make a deep copy
-        for ( int i = 0; i < blocks.length; i++ )
-        {
-            for ( int j = 0; j < blocks[ i ].length; j++ )
-            {
-                current[ i ][ j ] = blocks[ i ][ j ];
-            }
-        }
-    }
+	// construct a board from an N-by-N array of blocks
+	// (where blocks[i][j] = block in row i, column j)
+	public Board( int[][] blocks )
+	{
+		int length = blocks.length;
+		this.blocks = new int[ length ][ length ];
+		this.goal = new int[ length ][ length ];
+		int k = 1;
+		// make a deep copy
+		for ( int i = 0; i < blocks.length; i++ )
+		{
+			for ( int j = 0; j < blocks[ i ].length; j++ )
+			{
+				this.blocks[ i ][ j ] = blocks[ i ][ j ];
+				goal[ i ][ j ] = k++;
+			}
+		}
+		goal[ length - 1 ][ length - 1 ] = 0;
+		//		previousBoard = null;
+	}
 
-    // board dimension N
-    public int dimension()
-    {
-        return current.length;
-    }
-
-
-    // number of blocks out of place
-    public int hamming()
-    {
-        return 0;
-    }
-
-    // sum of Manhattan distances between blocks and goal
-    public int manhattan()
-    {
-
-        return 0;
-    }
-
-    // is this board the goal board?
-    public boolean isGoal()
-    {
-
-        return false;
-    }
-
-    // a board that is obtained by exchanging any pair of blocks
-    public Board twin()
-    {
-
-        return null;
-    }
-
-    // does this board equal y?
-    public boolean equals( Object y )
-    {
-
-        return false;
-    }
-
-    // all neighboring boards
-    public Iterable<Board> neighbors()
-    {
+	// board dimension N
+	public int dimension()
+	{
+		return blocks.length;
+	}
 
 
-        return null;
-    }
+	// number of blocks out of place
+	public int hamming()
+	{
+		int hammingCount = moves;
+		for ( int i = 0; i < dimension(); i++ )
+		{
+			for ( int j = 0; j < dimension(); j++ )
+			{
+				if ( blocks[ i ][ j ] != goal[ i ][ j ] && blocks[ i ][ j ] != 0 )
+				{
+					hammingCount++;
+				}
+			}
+		}
+
+		return hammingCount;
+	}
+
+	// sum of Manhattan distances between blocks and goal
+	public int manhattan()
+	{
+		int manhattan = moves;
+		int colGoal;
+		int rowGoal;
+		for ( int row = 0; row < dimension(); row++ )
+		{
+			for ( int col = 0; col < dimension(); col++ )
+			{
+				if ( blocks[ row ][ col ] != 0 )
+				{
+					colGoal = ( blocks[ row ][ col ] - 1 ) % dimension();
+					rowGoal = ( blocks[ row ][ col ] - 1 ) / dimension();
+					manhattan += Math.abs( colGoal - col );
+					manhattan += Math.abs( rowGoal - row );
+				}
+			}
+		}
+
+		return manhattan;
+	}
+
+	// is this board the goal board?
+	public boolean isGoal()
+	{
+		for ( int i = 0; i < dimension(); i++ )
+		{
+			for ( int j = 0; j < dimension(); j++ )
+			{
+				if ( blocks[ i ][ j ] != goal[ i ][ j ] )
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// a board that is obtained by exchanging any pair of blocks
+	public Board twin()
+	{
+		int[][] twinBlocks = new int[ dimension() ][ dimension() ];
+		for ( int i = 0; i < dimension(); i++ )
+		{
+			for ( int j = 0; j < dimension(); j++ )
+			{
+				twinBlocks[ i ][ j ] = blocks[ i ][ j ];
+			}
+		}
+
+		int temp = twinBlocks[ 0 ][ 0 ];
+		twinBlocks[ 0 ][ 0 ] = twinBlocks[ 0 ][ 1 ];
+		twinBlocks[ 0 ][ 1 ] = temp;
+
+		return new Board( twinBlocks );
+	}
+
+	// does this board equal y?
+	public boolean equals( Object y )
+	{
+		if ( y == null )
+		{
+			return false;
+		}
+		if ( !( y instanceof Board ) )
+		{
+			return false;
+		}
+		if ( y == this )
+		{
+			return true;
+		}
+		if ( ( (Board) y ).dimension() != this.dimension() )
+		{
+			return false;
+		}
+		if ( this.toString().equals( y.toString() ) )
+		{
+			return true;
+		}
+		return false;
+	}
+
+	// all neighboring boards
+	public Iterable<Board> neighbors()
+	{
+		int[] space = findSpace();
+		int row = space[ 0 ];
+		int col = space[ 1 ];
+
+		Board left = null;
+		Board right = null;
+		Board top = null;
+		Board bottom = null;
+
+		if ( col != 0 )
+		{
+			blocks[ row ][ col ] = blocks[ row ][ col - 1 ];
+			blocks[ row ][ col - 1 ] = 0;
+			left = new Board( blocks );
+			blocks[ row ][ col - 1 ] = blocks[ row ][ col ];
+			blocks[ row ][ col ] = 0;
+		}
+
+		if ( col != dimension() - 1 )
+		{
+			blocks[ row ][ col ] = blocks[ row ][ col + 1 ];
+			blocks[ row ][ col + 1 ] = 0;
+			right = new Board( blocks );
+			blocks[ row ][ col + 1 ] = blocks[ row ][ col ];
+			blocks[ row ][ col ] = 0;
+		}
+
+		if ( row != 0 )
+		{
+			blocks[ row ][ col ] = blocks[ row - 1 ][ col ];
+			blocks[ row - 1 ][ col ] = 0;
+			top = new Board( blocks );
+			blocks[ row - 1 ][ col ] = blocks[ row ][ col ];
+			blocks[ row ][ col ] = 0;
+		}
+
+		if ( row != dimension() - 1 )
+		{
+			blocks[ row ][ col ] = blocks[ row + 1 ][ col ];
+			blocks[ row + 1 ][ col ] = 0;
+			right = new Board( blocks );
+			blocks[ row + 1 ][ col ] = blocks[ row ][ col ];
+			blocks[ row ][ col ] = 0;
+		}
+
+		List<Board> neighbors = new ArrayList<Board>();
+		if ( left != null )
+		{
+			neighbors.add( left );
+		}
+		if ( right != null )
+		{
+			neighbors.add( right );
+		}
+		if ( top != null )
+		{
+			neighbors.add( top );
+		}
+		if ( bottom != null )
+		{
+			neighbors.add( left );
+		}
+		return neighbors;
+	}
+
+	private int[] findSpace()
+	{
+		for ( int i = 0; i < dimension(); i++ )
+		{
+			for ( int j = 0; j < dimension(); j++ )
+			{
+				if ( blocks[ i ][ j ] == 0 )
+				{
+					int[] space = new int[ 2 ];
+					space[ 0 ] = i;
+					space[ 1 ] = j;
+					return space;
+				}
+			}
+
+		}
+		return null;
+	}
 
 
-    // string representation of this board (in the output format specified below)
-    public String toString()
-    {
-        StringBuilder output = new StringBuilder( String.format( "%d\n", dimension() ) );
-        for ( int i = 0; i < current.length; i++ )
-        {
-            for ( int j = 0; j < current.length; j++ )
-            {
-                output.append( String.format( "%3d", current[ i ][ j ] ) );
-            }
-            output.append( "\n" );
-        }
-        return output.toString();
-    }
+	// string representation of this board (in the output format specified below)
+	public String toString()
+	{
+		StringBuilder output = new StringBuilder( String.format( "%d\n", dimension() ) );
+		for ( int i = 0; i < blocks.length; i++ )
+		{
+			for ( int j = 0; j < blocks.length; j++ )
+			{
+				output.append( String.format( "%3d", blocks[ i ][ j ] ) );
+			}
+			output.append( "\n" );
+		}
+		return output.toString();
+	}
 
-    // unit tests (not graded)
-    public static void main( String[] args )
-    {
+	// unit tests (not graded)
+	public static void main( String[] args )
+	{
 
-    }
+	}
 }
--- Week4/src/test/java/BoardTest.java
+++ Week4/src/test/java/BoardTest.java
@@ -2,83 +2,125 @@ import edu.princeton.cs.algs4.StdOut;
 import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by cstanwood on 30/09/15.
  */
 public class BoardTest
 {
-    Board simple3x3Board;
+	Board simple3x3Board1;
+	Board twin3x3Board;
+	Board simple3x3Board2;
+	Board simple2x2Board;
+	Board reverse3x3Board;
+	Board hole3x3Board;
 
-    @Before
-    public void setUp() throws Exception
-    {
-        int[][] blocks = new int[3][3];
-        int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
-        int k = 0;
-        for ( int i = 0; i < blocks.length; i++ )
-        {
-            for ( int j = 0; j < blocks.length; j++ )
-            {
-                blocks[ i ][ j ] = ints[ k++ ];
-            }
-        }
-        simple3x3Board = new Board( blocks );
-    }
+	@Before
+	public void setUp() throws Exception
+	{
+		int[][] blocks3x3 = new int[ 3 ][ 3 ];
+		int[][] twin3x3 = new int[ 3 ][ 3 ];
+		int[][] reverseBlocks = new int[ 3 ][ 3 ];
+		int[][] blocks2x2 = new int[ 2 ][ 2 ];
+		int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 0};
+		int k = 0;
+		int l = 8;
+		for ( int i = 0; i < blocks3x3.length; i++ )
+		{
+			for ( int j = 0; j < blocks3x3.length; j++ )
+			{
+				blocks3x3[ i ][ j ] = ints[ k ];
+				twin3x3[ i ][ j ] = ints[ k++ ];
+				reverseBlocks[ i ][ j ] = ints[ l-- ];
+			}
+		}
+		twin3x3[ 0 ][ 0 ] = 2;
+		twin3x3[ 0 ][ 1 ] = 1;
+		k = 0;
+		for ( int i = 0; i < blocks2x2.length; i++ )
+		{
+			for ( int j = 0; j < blocks2x2.length; j++ )
+			{
+				blocks2x2[ i ][ j ] = ints[ k++ ];
+			}
+		}
+		blocks2x2[ blocks2x2.length - 1 ][ blocks2x2.length - 1 ] = 0;
 
-    @org.junit.Test
-    public void testDimension() throws Exception
-    {
-        assertEquals( 3, simple3x3Board.dimension() );
-    }
+		simple3x3Board1 = new Board( blocks3x3 );
+		simple3x3Board2 = new Board( blocks3x3 );
+		reverse3x3Board = new Board( reverseBlocks );
+		simple2x2Board = new Board( blocks2x2 );
+		twin3x3Board = new Board( twin3x3 );
+		int[][] hole3x3 = {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}};
+		hole3x3Board = new Board( hole3x3 );
+	}
 
-    @org.junit.Test
-    public void testHamming() throws Exception
-    {
+	@org.junit.Test
+	public void testDimension() throws Exception
+	{
+		assertEquals( 3, simple3x3Board1.dimension() );
+	}
 
-    }
+	@org.junit.Test
+	public void testHamming() throws Exception
+	{
+		assertTrue( simple3x3Board1.hamming() == 0 );
+		assertTrue( reverse3x3Board.hamming() == 7 );
+		assertTrue( hole3x3Board.hamming() == 5 );
 
-    @org.junit.Test
-    public void testManhattan() throws Exception
-    {
+	}
 
-    }
+	@org.junit.Test
+	public void testManhattan() throws Exception
+	{
+		assertTrue( hole3x3Board.manhattan() == 10 );
+		assertTrue( simple3x3Board1.manhattan() == 0 );
+		assertTrue( reverse3x3Board.manhattan() == 20 );
+	}
 
-    @org.junit.Test
-    public void testIsGoal() throws Exception
-    {
+	@org.junit.Test
+	public void testIsGoal() throws Exception
+	{
+		assertTrue( simple3x3Board1.isGoal() );
+		assertFalse( reverse3x3Board.isGoal() );
+	}
 
-    }
+	@org.junit.Test
+	public void testTwin() throws Exception
+	{
+		assertFalse( simple3x3Board1.twin().equals( simple3x3Board2 ) );
+		assertTrue( simple3x3Board1.twin().equals( twin3x3Board ) );
+	}
 
-    @org.junit.Test
-    public void testTwin() throws Exception
-    {
+	@org.junit.Test
+	public void testEquals() throws Exception
+	{
+		assertFalse( simple3x3Board1.equals( null ) );
+		assertFalse( simple3x3Board1.equals( new Integer( 1 ) ) );
+		assertFalse( simple3x3Board1.equals( simple2x2Board ) );
+		assertTrue( simple3x3Board1.equals( simple3x3Board1 ) );
+		assertTrue( simple3x3Board1.equals( simple3x3Board2 ) );
+	}
 
-    }
+	@org.junit.Test
+	public void testNeighbors() throws Exception
+	{
 
-    @org.junit.Test
-    public void testEquals() throws Exception
-    {
+	}
 
-    }
+	@org.junit.Test
+	public void testToString() throws Exception
+	{
+		StdOut.print( simple3x3Board1.toString() );
+		assertEquals( "3\n  1  2  3\n  4  5  6\n  7  8  0\n", simple3x3Board1.toString() );
 
-    @org.junit.Test
-    public void testNeighbors() throws Exception
-    {
+	}
 
-    }
+	@org.junit.Test
+	public void testMain() throws Exception
+	{
 
-    @org.junit.Test
-    public void testToString() throws Exception
-    {
-        StdOut.print( simple3x3Board.toString() );
-        assertEquals( "3\n  1  2  3\n  4  5  6\n  7  8  0\n", simple3x3Board.toString() );
-
-    }
-
-    @org.junit.Test
-    public void testMain() throws Exception
-    {
-
-    }
+	}
 }
